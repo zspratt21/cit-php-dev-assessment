13f820106e1be71318b656f70647f669063893f5
Zak <zspratt21@gmail.com>
2024-04-02 17:34:05 +0800

    Refactored task 2 script to utilize the minicli framework.
    Added db dependency to cli container.

---
+++ app/Command/Foobar/DefaultController.php
@@ -0,0 +1,43 @@
+<?php
+
+/**
+ * Default Controller for Foobar Command.
+ *
+ * Implements the logic to display the foobar table.
+ */
+
+declare(strict_types=1);
+
+namespace App\Command\Foobar;
+
+use Minicli\Command\CommandController;
+use Minicli\Output\Helper\TableHelper;
+
+class DefaultController extends CommandController
+{
+    public function handle(): void
+    {
+        $limit = 100;
+        $this->display('Foobar Value Table');
+        $table = new TableHelper();
+        $table->addHeader(['Value', 'Output']);
+        for ($i = 1; $i <= $limit; $i++) {
+            // first check determines if the value of $i is divisible by 3
+            $check_a = 0 === $i % 3;
+            // second check determines if the value of $i is divisible by 5
+            $check_b = 0 === $i % 5;
+            $table->addRow([(string)$i, match (true) {
+                // print foobar if both checks are true
+                $check_a && $check_b => "foobar",
+                // print foo if only the first check is true
+                $check_a => "foo",
+                // print bar if only the second check is true
+                $check_b => "bar",
+                // print the value of $i if none of the checks are true
+                default => (string)$i,
+            }]);
+        }
+        $this->rawOutput($table->getFormattedTable());
+        $this->newline();
+    }
+}
--- foobar.php
+++ foobar.php
@@ -7,21 +7,28 @@ declare(strict_types=1);
  * This script implements task 2 of the assessment.
  */
 
-$limit = 100;
+require 'vendor/autoload.php';
 
-for ($i = 1; $i <= $limit; $i++) {
-    // first check determines if the value of $i is divisible by 3
-    $check_a = 0 === $i % 3;
-    // second check determines if the value of $i is divisible by 5
-    $check_b = 0 === $i % 5;
-    echo match (true) {
-        // print foobar if both checks are true
-        $check_a && $check_b => "foobar \n",
-        // print foo if only the first check is true
-        $check_a => "foo \n",
-        // print foobar if both checks are true
-        $check_b => "bar \n",
-        // print the value of $i if none of the checks are true
-        default => $i."\n",
-    };
+use Minicli\App;
+use Minicli\Exception\CommandNotFoundException;
+
+$app = new App();
+
+try {
+    $app->runCommand(['', 'foobar']);
+} catch (CommandNotFoundException $notFoundException) {
+    $app->error("Command Not Found.");
+    return 1;
+} catch (Throwable $e) {
+    if ($e instanceof Exception) {
+        if ($app->config->debug) {
+            $app->error("An error occurred:");
+            $app->error($e->getMessage());
+        }
+    } else {
+        $app->info('An error occurred: '.$e->getMessage());
+    }
+    return 1;
 }
+
+return 0;
